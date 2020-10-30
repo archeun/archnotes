259eca5ff76533e76e3424c8b9e0f4fe2f50b463
ArunaTebel <arunatebel@hotmail.com>
2020-10-30 21:43:15 +0530

    auth utility refactored to be used as an object

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,24 @@
 import React from 'react'
-import {Layout, Row, Col, Menu, Button} from 'antd';
+import {Layout, Row, Col, Button} from 'antd';
 import classnames from 'classnames';
-import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 import styles from '../styles/Home.module.css'
 import dynamic from 'next/dynamic'
 import Head from 'next/head'
-import {initSignInButton, signOut, onAuthStateChange} from "../util/arch-auth";
+import ArchAuth from "../util/arch-auth";
 
 const {Content} = Layout;
 const ArchNotesEditor = dynamic(() => import('../components/arch-notes-editor'), {ssr: false});
+const ArchNotesList = dynamic(() => import('../components/arch-notes-list'), {ssr: false});
 
 class Home extends React.Component {
 
     state = {loggedInUser: null};
 
     componentDidMount() {
-        initSignInButton('g-signin2');
-        onAuthStateChange((firebaseUser) => {
+        ArchAuth.initSignInButton('g-signin2');
+        ArchAuth.onAuthStateChange((firebaseUser) => {
             this.setState({loggedInUser: firebaseUser});
         });
     }
@@ -27,7 +27,7 @@ class Home extends React.Component {
         return (
             <div>
                 <Head>
-                    <script src="https://apis.google.com/js/platform.js" async defer></script>
+                    <script src="https://apis.google.com/js/platform.js" async defer/>
                     <meta name="google-signin-client_id" content="914280926964-idhkbcpv3irnsf9kbuac9sfjtp9j3bt5.apps.googleusercontent.com"/>
                     <meta name="google-signin-cookiepolicy" content="single_host_origin"/>
                     <meta name="google-signin-scope" content="profile email"/>
@@ -38,8 +38,8 @@ class Home extends React.Component {
                             <div className={styles.brandNameText}>archeun | NOTES</div>
                         </Col>
                         <Col span={6}>
-                            {this.state.loggedInUser ? '' : <div id="g-signin2" className={styles.signInOut} data-onsuccess="onSignIn" data-theme="dark"/>}
-                            <Button className={styles.signInOut} onClick={signOut}>
+                            {this.state.loggedInUser ? '' : <div id="g-signin2" className={styles.signInOut} data-theme="dark"/>}
+                            <Button className={styles.signInOut} onClick={ArchAuth.signOut}>
                                 {this.state.loggedInUser ? 'Logout' : ''}
                             </Button>
                             <div className={styles.loggedInUserName}>{this.state.loggedInUser ? this.state.loggedInUser.displayName : ''}</div>
@@ -47,7 +47,9 @@ class Home extends React.Component {
                     </Row>
                     <Content className={styles.content}>
                         <Row className={styles.contentRow}>
-                            <Col className={classnames(styles.contentRowLeftCol)} span={4}>Note List</Col>
+                            <Col className={classnames(styles.contentRowLeftCol)} span={4}>
+                                <ArchNotesList/>
+                            </Col>
                             <Col className={classnames(styles.contentRowRightCol)} span={20}>
                                 <ArchNotesEditor/>
                             </Col>
--- util/arch-auth.js
+++ util/arch-auth.js
@@ -1,45 +1,5 @@
 import firebase from "firebase";
 
-export function signIn(googleUser) {
-    // We need to register an Observer on Firebase Auth to make sure auth is initialized.
-    var unsubscribe = firebase.auth().onAuthStateChanged(function (firebaseUser) {
-        unsubscribe();
-        // Check if we are already signed-in Firebase with the correct user.
-        if (!isUserEqual(googleUser, firebaseUser)) {
-            // Build Firebase credential with the Google ID token.
-            var credential = firebase.auth.GoogleAuthProvider.credential(googleUser.getAuthResponse().id_token);
-            // Sign in with credential from the Google user.
-            firebase.auth().signInWithCredential(credential)
-                .then(function () {
-                })
-                .catch(function (error) {
-                });
-        } else {
-            console.log('User already signed-in Firebase.');
-        }
-    });
-}
-
-export function signOut() {
-    const auth2 = gapi.auth2.getAuthInstance();
-    firebase.auth().signOut().then(function () {
-        auth2.disconnect();
-        window.location.reload();
-    }).catch(function (error) {
-    });
-}
-
-export function onAuthStateChange(callback) {
-    firebase.auth().onAuthStateChanged(callback);
-}
-
-export function initSignInButton(buttonId) {
-    window.gapi.signin2.render(buttonId, {
-        'scope': 'profile email',
-        'onsuccess': signIn
-    });
-}
-
 function isUserEqual(googleUser, firebaseUser) {
     if (firebaseUser) {
         let providerData = firebaseUser.providerData;
@@ -53,3 +13,43 @@ function isUserEqual(googleUser, firebaseUser) {
     }
     return false;
 }
+
+const ArchAuth = {
+    signIn: (googleUser) => {
+        // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+        let unsubscribe = firebase.auth().onAuthStateChanged((firebaseUser) => {
+            unsubscribe();
+            // Check if we are already signed-in Firebase with the correct user.
+            if (!isUserEqual(googleUser, firebaseUser)) {
+                // Build Firebase credential with the Google ID token.
+                let credential = firebase.auth.GoogleAuthProvider.credential(googleUser.getAuthResponse().id_token);
+                // Sign in with credential from the Google user.
+                firebase.auth().signInWithCredential(credential).then(() => {
+                }).catch((error) => {
+                });
+            }
+        });
+    },
+
+    signOut: () => {
+        const auth2 = gapi.auth2.getAuthInstance();
+        firebase.auth().signOut().then(() => {
+            auth2.disconnect();
+            window.location.reload();
+        }).catch((error) => {
+        });
+    },
+
+    onAuthStateChange: (callback) => {
+        firebase.auth().onAuthStateChanged(callback);
+    },
+
+    initSignInButton: (buttonId) => {
+        window.gapi.signin2.render(buttonId, {
+            'scope': 'profile email',
+            'onsuccess': ArchAuth.signIn
+        });
+    }
+};
+
+export default ArchAuth;
